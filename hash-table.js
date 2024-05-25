937f3de7433f41745392e94cc1e6ff686d677119
huss2012 <huss24940@gmail.com>
2024-05-25 14:53:43 +0300

    Non-optimized solution of hash instance method has been written.

--- hash-table.js
+++ hash-table.js
@@ -19,6 +19,29 @@ class HashTable {
 
   hash(key) {
     // Your code here
+    let letters = ["a", "b", "c", "d", "e", "f"];
+    let lettersHexValues = [10, 11, 12, 13, 14, 15];
+    let count = 0;
+    let sha = sha256(key);//64hexchar
+    console.log(sha);
+
+    for (let i = 7; i >= 0; i--){
+      let hexChar = sha[i];
+      console.log(hexChar);
+      if (letters.includes(hexChar)) {//if true it is a letter find the value.
+
+        for (let j = 0; j < letters.length; j++){
+          let letterValue = letters[j];
+
+          if (letterValue === hexChar) {
+            count += Math.pow(16, 7 - i) * lettersHexValues[j];
+          }
+        }
+      } else {//if it is a number 0-9
+        count += Math.pow(16, 7 - i) * hexChar;
+      }
+    }
+    return count;
   }
 
   hashMod(key) {
